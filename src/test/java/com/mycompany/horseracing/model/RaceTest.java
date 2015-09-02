5a7d9cc6523a2508624fd5b5bcf1bad6c4709a74
Colin But <colinbut@users.noreply.github.com>
2015-09-02 20:08:33 +0100

    Mockito support

--- src/test/java/com/mycompany/horseracing/model/RaceTest.java
+++ src/test/java/com/mycompany/horseracing/model/RaceTest.java
@@ -5,6 +5,15 @@
  */
 package com.mycompany.horseracing.model;
 
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 import com.mycompany.horseracing.domain.Horse;
 import com.mycompany.horseracing.domain.Race;
 
@@ -14,9 +23,21 @@ import com.mycompany.horseracing.domain.Race;
  */
 public class RaceTest {
 
+	@Mock
+	List<Horse> horses;
+	
+	@InjectMocks
 	private Race race = new Race();
 	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
 	public void testAddHorse() {
-		race.addHorse(new Horse());
+		Horse horse = new Horse();
+		race.addHorse(horse);
+		Mockito.verify(horses, Mockito.times(1)).add(horse);
 	}
 }
